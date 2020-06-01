1817aa5a4650f13e3b7f4d50123545f4de77539d
RobSinclair <robert.sinclair@informed.com>
2020-06-01 14:56:06 +0100

    some date stuff

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ const routes = require('./app/routes.js')
 const utils = require('./lib/utils.js')
 const extensions = require('./lib/extensions/extensions.js')
 
+var dateFilter = require('nunjucks-date-filter');
+
 // Variables for v6 backwards compatibility
 // Set false by default, then turn on if we find /app/v6/routes.js
 var useV6 = false
@@ -96,6 +98,9 @@ if (env === 'development') {
 }
 
 nunjucksConfig.express = app
+
+var nunjucksAppEnv = nunjucks.configure(appViews, nunjucksConfig)
+nunjucksAppEnv.addFilter('date', dateFilter);
 
 var nunjucksAppEnv = nunjucks.configure(appViews, nunjucksConfig)
 
