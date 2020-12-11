9c5c9514bd82adeda22dadb88c58b45caf400bee
Bristow <bristowh@informed.co.uk>
2020-12-11 15:36:16 +0000

    start page updates

--- app/routes.js
+++ app/routes.js
@@ -1011,7 +1011,7 @@ router.post('/accounts-march2', function (req, res) {
   } else if (cazaction == 'sign-in') {
       res.redirect('https://accountpay.cleanairzone.defra.gov.uk/users/sign_in')
   } else if (cazaction == 'pay-iod') {
-      res.redirect('https://t2m.io/DzEChgua')
+      res.redirect('https://t2m.io/kFzu1vX9')
   } else {
       res.render('start-pages/start-accounts-march-2', {
           error: true,
@@ -1054,7 +1054,7 @@ router.post('/accounts-marchaltpay', function (req, res) {
   var cazaction = req.body['cazaction'];
 
   if (cazaction == 'pay-one') {
-          res.redirect('https://t2m.io/DzEChgua')
+          res.redirect('https://t2m.io/kFzu1vX9')
   } else if (cazaction == 'pay-multiple') {
       res.redirect('https://accountpay.cleanairzone.defra.gov.uk/users/sign_in')
   } else {
